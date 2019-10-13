f51ac357d5b935b462e23016eed1b32414f8daca
Andrey Borisov <andrey.borisov@comodo.com>
2019-10-13 23:54:49 +0300

    add 'is element present' method

--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -3,6 +3,9 @@ from .base_page import BasePage
 
 
 class MainPage(BasePage):
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), 'Login link is not presented'
+
     def go_to_login_page(self):
         link_ = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         link_.click()
